16fd2c2adbfb3b6e001fb2a27247c5ebd145fa63
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-08 14:08:10 +0900

    [BOOK-109] feat: add icon name space

--- src/Projects/BKDesign/Sources/Foundation/GraphicSystem/BKIcon.swift
+++ src/Projects/BKDesign/Sources/Foundation/GraphicSystem/BKIcon.swift
@@ -1,3 +1,36 @@
 // Copyright © 2025 Booket. All rights reserved
 
-import Foundation
+import UIKit
+
+public enum BKIcon: String{
+    case alert = "alert-circle"
+    case appleLogo = "apple"
+    case bellOff = "bell-off"
+    case bell = "bell"
+    case bookOpen = "book-open"
+    case bookmark = "bookmark"
+    case check = "check"
+    case chevronDown = "chevron-down"
+    case chevronUp = "chevron-up"
+    case chevronLeft = "chevron-left"
+    case chevronRight = "chevron-right"
+    case editMemo = "edit"
+    case edit = "edit-2"
+    case editLine = "edit-3"
+    case kakaoLogo = "kakao"
+    case loader = "loader"
+    case maximize = "maximize"
+    case menu = "menu"
+    case moreHorizontal = "more-horizontal"
+    case moreVertical = "more-vertical"
+    case plus = "plus"
+    case search = "search"
+    case settings = "settings"
+    case star = "star"
+    case xmark = "x"
+
+    /// 해당 아이콘 이름을 가진 UIImage를 반환합니다.
+    public var image: UIImage? {
+        return UIImage(named: self.rawValue)
+    }
+}
