eee7ba934ac5376d158a7d8d3c0cefc9d7691aac
Jiyong Jung <hickerd@sch.ac.kr>
2025-07-09 13:32:49 +0900

    [BOOK-110] fix: resolve reviews

--- src/Projects/BKData/Sources/Interface/Network/RefreshHandler.swift
+++ src/Projects/BKData/Sources/Interface/Network/RefreshHandler.swift
@@ -5,5 +5,5 @@ import Combine
 import Foundation
 
 public protocol RefreshHandler {
-    func refresh(token accessToken: String) -> AnyPublisher<Void, AuthError>
+    func refresh(token refreshToken: String) -> AnyPublisher<Void, AuthError>
 }
