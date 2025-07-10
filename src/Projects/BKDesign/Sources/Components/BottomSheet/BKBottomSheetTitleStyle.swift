af2c0a5be23d27c2a33410e96a85a35e89a72f36
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-11 02:34:19 +0900

    [BOOK-115] feat: add BottomSheet Title style enum

---
+++ src/Projects/BKDesign/Sources/Components/BottomSheet/BKBottomSheetTitleStyle.swift
@@ -0,0 +1,27 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import UIKit
+
+/// 바텀시트의 타이틀 스타일을 정의합니다.
+public enum BKBottomSheetTitleStyle {
+    /// 타이틀 없음
+    case none
+
+    /// leading 정렬 - 타이틀만
+    case title(_ title: String)
+
+    /// leading 정렬 - 타이틀 + X 버튼
+    case titleWithCloseButton(title: String)
+
+    /// leading 정렬 - 타이틀 + 서브타이틀
+    case titleWithSubtitle(title: String, subtitle: String)
+
+    /// leading 정렬 - 타이틀 + 서브타이틀 + X 버튼
+    case titleWithSubtitleAndCloseButton(title: String, subtitle: String)
+
+    /// center 정렬 - 타이틀만
+    case centerTitle(_ title: String)
+
+    /// center 정렬 - 타이틀 + 서브타이틀
+    case centerTitleWithSubtitle(title: String, subtitle: String)
+}
