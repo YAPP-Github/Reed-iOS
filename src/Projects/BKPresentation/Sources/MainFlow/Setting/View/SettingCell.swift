0ea653320d5e1b0ca17dce24f021520adb59f677
Jiyong Jung <hickerd@sch.ac.kr>
2025-07-11 20:44:13 +0900

    [BOOK-130] feat: add required subviews to build SettingView layout

---
+++ src/Projects/BKPresentation/Sources/MainFlow/Setting/View/SettingCell.swift
@@ -0,0 +1,83 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import BKDesign
+import SnapKit
+import UIKit
+
+final class SettingCell: UICollectionViewListCell {
+    enum SettingCellStyle {
+        case chevron
+        case label
+        case none
+    }
+    static let identifier = "SettingCell"
+    
+    private let titleLabel = BKLabel()
+    private let versionLabel = BKLabel(
+        fontStyle: .body1(weight: .medium),
+        color: .bkContentColor(
+            .secondary
+        )
+    )
+    private let iconView: UIImageView = {
+        let view = UIImageView(image: BKImage.Icon.chevronRight)
+        view.tintColor = .bkContentColor(.secondary)
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        [titleLabel, iconView, versionLabel].forEach(addSubview(_:))
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+                .inset(BKInset.inset20)
+            $0.centerY.equalToSuperview()
+        }
+
+        iconView.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(BKInset.inset3)
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(BKLayoutSize.icon)
+        }
+
+        versionLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(BKInset.inset3)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func preferredLayoutAttributesFitting(
+        _ layoutAttributes: UICollectionViewLayoutAttributes
+    ) -> UICollectionViewLayoutAttributes {
+        let attrs = super.preferredLayoutAttributesFitting(layoutAttributes)
+        attrs.frame.size.height = BKLayoutSize.Height.cell
+        return attrs
+    }
+    
+    func configure(
+        title: String,
+        style: SettingCellStyle = .chevron,
+        appVersion: String? = nil
+    ) {
+        titleLabel.setText(text: title)
+        
+        switch style {
+        case .chevron:
+            iconView.isHidden = false
+            versionLabel.isHidden = true
+        case .label:
+            iconView.isHidden = true
+            versionLabel.isHidden = false
+            versionLabel.setText(text: appVersion ?? "-")
+        case .none:
+            iconView.isHidden = true
+            versionLabel.isHidden = true
+        }
+    }
+}
