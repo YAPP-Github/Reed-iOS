a4a4d8a1cbb1740e34cd6a461c17f1b601d12341
Jiyong Jung <hickerd@sch.ac.kr>
2025-07-11 20:43:18 +0900

    [BOOK-130] feat: add BKLayoutSize

---
+++ src/Projects/BKPresentation/Sources/Common/Constant/BKLayoutSize.swift
@@ -0,0 +1,16 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import Foundation
+
+/// 디자인 시스템으로 가기 보다는, 특정 뷰들에 대해서 사용될 상수를 정의합니다.
+enum BKLayoutSize {
+    static let icon: CGFloat = 24
+    
+    enum Width {
+        
+    }
+    
+    enum Height {
+        static let cell: CGFloat = 56
+    }
+}
