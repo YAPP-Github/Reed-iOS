fc38c96e9ae8ccaa3377504a8d971245ab3472c0
Jiyong Jung <hickerd@sch.ac.kr>
2025-07-15 10:28:40 +0900

    [BOOK-125] fix: remove unnecessary settings in SceneDelegate

--- src/Projects/Booket/Sources/SceneDelegate.swift
+++ src/Projects/Booket/Sources/SceneDelegate.swift
@@ -58,34 +58,16 @@ private extension SceneDelegate {
     }
     
     func setupNavigationBar() {
-        guard let font = BKTextStyle.headline2(weight: .semiBold).uiFont else { return }
         let appearance = UINavigationBarAppearance()
         appearance.configureWithDefaultBackground()
-        appearance.titleTextAttributes = [
-            .foregroundColor: UIColor.bkContentColor(.primary),
-            .font: font
-        ]
         appearance.backgroundColor = .bkBaseColor(.primary)
         appearance.shadowColor = .clear
         
-        let barButtonAppearance = UIBarButtonItemAppearance()
-        barButtonAppearance.normal.titleTextAttributes = [
-            .foregroundColor: UIColor.clear
-        ]
-        
-        let backButtonImage = BKImage.Icon.chevronLeft
-            .withRenderingMode(.alwaysTemplate)
-            .withAlignmentRectInsets(
-                UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
-            )
-        
-        appearance.backButtonAppearance = barButtonAppearance
-        appearance.setBackIndicatorImage(backButtonImage, transitionMaskImage: backButtonImage)
-        
         navigationController.navigationBar.tintColor = .bkContentColor(.primary)
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.navigationItem.largeTitleDisplayMode = .never
         navigationController.navigationBar.standardAppearance = appearance
         navigationController.navigationBar.scrollEdgeAppearance = appearance
         navigationController.navigationBar.compactAppearance = appearance
-        navigationController.navigationBar.isTranslucent = false
     }
 }
