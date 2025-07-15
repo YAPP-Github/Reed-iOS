df3678b7d0a90670079d04cfcaacba43b5a921a5
Jiyong Jung <hickerd@sch.ac.kr>
2025-07-15 10:28:26 +0900

    [BOOK-125] refactor: enforce navigation bar style in BaseViewController

--- src/Projects/BKPresentation/Sources/Common/Component/BaseViewController.swift
+++ src/Projects/BKPresentation/Sources/Common/Component/BaseViewController.swift
@@ -2,9 +2,18 @@
 
 import UIKit
 
-class BaseViewController<T: BaseView>: UIViewController {
+///
+class BaseViewController<T: BaseView>: UIViewController, BKNavigationBarStylable {
     // MARK: - Properties
     let contentView: T
+    
+    open var bkNavigationBarStyle: UINavigationController.BKNavigationBarStyle {
+        fatalError("Subclasses must override bkNavigationBarStyle")
+    }
+
+    open var bkNavigationTitle: String {
+        fatalError("Subclasses must override bkNavigationTitle")
+    }
     
     // MARK: - Initialize
     init() {
@@ -33,6 +42,13 @@ class BaseViewController<T: BaseView>: UIViewController {
         self.setupLayout()
         self.bindAction()
         self.bindState()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let _ = self as? BKNavigationBarStylable {
+            navigationController?.applyStyleIfNeeded(for: self)
+        }
     }
     
     // MARK: - Common Methods
