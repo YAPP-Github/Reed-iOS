1232f012230a21f3c81d5c55bd9e2810cef19f6d
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-17 02:31:10 +0900

    [BOOK-134] feat: TermsView 상단 부분 UI 구현 및 확인

--- src/Projects/BKPresentation/Sources/AuthFlow/View/TermsView.swift
+++ src/Projects/BKPresentation/Sources/AuthFlow/View/TermsView.swift
@@ -12,16 +12,73 @@ enum TermsViewEvent {
 
 final class TermsView: BaseView {
     
+    // MARK: - Layout Metrics
+    enum LayoutGuide {
+        static let innerViewHPadding: CGFloat = BKSpacing.spacing4
+        static let iconSize: CGFloat = BKSpacing.spacing6
+        static let verticalPadding: CGFloat = BKSpacing.spacing5
+        
+        static let titleTopInset: CGFloat = 60
+        static let agreeAreaTopInset: CGFloat = BKSpacing.spacing7
+        static let agreeAreaHeight: CGFloat = 66
+        
+    }
+    
     private let titleLabel = BKLabel(
         text: "약관 동의 후\n독서 기록을 남겨보세요",
         fontStyle: .title2(weight: .semiBold),
         alignment: .left
     )
     
+    private let agreeAllAreaView = UIView()
+    private let checkOnceBox = BKCheckBox(frame: .zero, type: .rectangle)
+    
+    private let agreeAllLabel = BKLabel(
+        fontStyle: .headline1(weight: .semiBold),
+        alignment: .left
+    )
+    
     override func setupView() {
         titleLabel.numberOfLines = 2
         
-        addSubviews(titleLabel)
+        setupAgreeAllAreaView()
+        addSubviews(titleLabel, agreeAllAreaView)
+    }
+    
+    override func setupLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(LayoutGuide.titleTopInset)
+            $0.leading.trailing.equalToSuperview().inset(LayoutGuide.verticalPadding)
+        }
+        
+        checkOnceBox.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(LayoutGuide.innerViewHPadding)
+            $0.width.height.equalTo(LayoutGuide.iconSize)
+            $0.centerY.equalToSuperview()
+        }
+        
+        agreeAllLabel.snp.makeConstraints {
+            $0.leading.equalTo(checkOnceBox.snp.trailing).offset(LayoutGuide.innerViewHPadding)
+            $0.top.bottom.equalToSuperview().inset(LayoutGuide.verticalPadding)
+        }
+        
+        agreeAllAreaView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(LayoutGuide.agreeAreaTopInset)
+            $0.leading.trailing.equalToSuperview().inset(LayoutGuide.verticalPadding)
+            $0.height.equalTo(LayoutGuide.agreeAreaHeight)
+        }
+        
+    }
+    
+    private func setupAgreeAllAreaView() {
+        checkOnceBox.isEnabled = true
+        agreeAllLabel.setText(text: "약관 전체동의")
+        
+        agreeAllAreaView.layer.borderWidth = 1
+        agreeAllAreaView.layer.borderColor = UIColor.bkBorderColor(.brand).cgColor
+        agreeAllAreaView.layer.cornerRadius = BKRadius.small
+        
+        agreeAllAreaView.addSubviews(checkOnceBox, agreeAllLabel)
     }
     
 }
