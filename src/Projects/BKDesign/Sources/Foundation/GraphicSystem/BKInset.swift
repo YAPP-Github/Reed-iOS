3e25c392699728e4423981d8300dd59b72d5f996
Jiyong Jung <hickerd@sch.ac.kr>
2025-07-17 19:25:44 +0900

    [BOOK-148] feat: add inset2

--- src/Projects/BKDesign/Sources/Foundation/GraphicSystem/BKInset.swift
+++ src/Projects/BKDesign/Sources/Foundation/GraphicSystem/BKInset.swift
@@ -4,6 +4,7 @@ import Foundation
 
 public enum BKInset {
     public static let inset1: CGFloat = 4
+    public static let inset2: CGFloat = 8
     public static let inset3: CGFloat = 12
     public static let inset3_2: CGFloat = 13
     public static let inset4: CGFloat = 16
