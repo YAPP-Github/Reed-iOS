c789e54c665e5146ef035e19842e00a1a60b6674
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-24 19:31:34 +0900

    [BOOK-160] feat: 내서재 임시 뷰컨트롤러, 코디네이터 생성

---
+++ src/Projects/BKPresentation/Sources/ArchiveFlow/ArchiveTempViewController.swift
@@ -0,0 +1,30 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import UIKit
+import SnapKit
+
+final class ArchiveTempViewController: UIViewController {
+    var coordinator: ArchiveCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        title = "내 서재"
+        
+        // 임시 라벨 추가
+        let label = UILabel()
+        label.text = "내 서재 화면"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        label.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
