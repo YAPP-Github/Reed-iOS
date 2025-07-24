ac759df764068f430a7f26a50be35155b836e638
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-24 19:31:34 +0900

    [BOOK-160] feat: 내서재 임시 뷰컨트롤러, 코디네이터 생성

---
+++ src/Projects/BKPresentation/Sources/ArchiveFlow/ArchiveCoordinator.swift
@@ -0,0 +1,28 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import UIKit
+
+final class ArchiveCoordinator: Coordinator {
+    weak var parentCoordinator: Coordinator?
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(
+        parentCoordinator: Coordinator?,
+        navigationController: UINavigationController
+    ) {
+        self.parentCoordinator = parentCoordinator
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let archiveViewController = ArchiveTempViewController()
+        archiveViewController.coordinator = self
+        navigationController.pushViewController(archiveViewController, animated: false)
+    }
+}
+
+extension ArchiveCoordinator {
+    // Archive 관련 네비게이션 메서드들을 여기에 추가
+    
+}
