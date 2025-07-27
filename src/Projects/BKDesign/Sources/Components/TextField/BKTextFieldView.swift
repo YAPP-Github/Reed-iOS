66c3413dc7ff13e8546416594479b5be5aa0b087
Jiyong Jung <hickerd@sch.ac.kr>
2025-07-27 19:00:17 +0900

    [BOOK-164] feat: add BKTextFieldView

---
+++ src/Projects/BKDesign/Sources/Components/TextField/BKTextFieldView.swift
@@ -0,0 +1,105 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import UIKit
+
+public final class BKTextFieldView: UIView {
+    private let titleLabel = BKLabel()
+    private let textField = BKBaseTextField()
+    private let helpMessageLabel = BKLabel(type: .help)
+    
+    private var labelText: String
+    private var placeholder: String
+    private var helpMessage: String
+    
+    private var isError: Bool = false {
+        didSet {
+            isError ? textField.setType(type: .error) : textField.setType(type: .normal)
+        }
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        return CGSize(
+            width: UIView.noIntrinsicMetric,
+            height: LayoutConstants.height
+        )
+    }
+    
+    public var text: String {
+        return textField.text ?? ""
+    }
+    
+    private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = BKSpacing.spacing2
+        stackView.alignment = .fill
+        return stackView
+    }()
+    
+    public init(
+        frame: CGRect = .zero,
+        labelText: String = "",
+        placeholder: String = "",
+        helpMessage: String = "",
+        isError: Bool = false
+    ) {
+        self.labelText = labelText
+        self.placeholder = placeholder
+        self.helpMessage = helpMessage
+        self.isError = isError
+        super.init(frame: frame)
+        setup()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        setContentHuggingPriority(.defaultLow, for: .horizontal)
+        setContentCompressionResistancePriority(.required, for: .horizontal)
+    }
+    
+    public func setHelpMessage(
+        _ message: String,
+        isError: Bool = false
+    ) {
+        helpMessageLabel.setText(text: message)
+        self.isError = isError
+    }
+}
+
+private extension BKTextFieldView {
+    func handleError() {
+        if isError {
+            textField.setType(type: .error)
+            helpMessageLabel.setColor(color: .bkContentColor(.error))
+        } else {
+            textField.setType(type: .normal)
+            helpMessageLabel.setColor(color: .bkContentColor(.tertiary))
+        }
+    }
+    
+    func setup() {
+        titleLabel.setText(text: labelText)
+        textField.placeholder = placeholder
+        helpMessageLabel.setText(text: helpMessage)
+        handleError()
+        
+        [titleLabel, textField, helpMessageLabel]
+            .forEach(stackView.addArrangedSubview(_:))
+        addSubview(stackView)
+    }
+    
+    func layout() {
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    enum LayoutConstants {
+        static let height: CGFloat = 108
+    }
+}
