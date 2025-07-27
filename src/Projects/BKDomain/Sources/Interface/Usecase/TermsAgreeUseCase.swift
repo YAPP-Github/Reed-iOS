4229934d416504dea1ee9918035b45467939c37c
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-27 17:05:44 +0900

    [BOOK-172] feat: usecase 작성

---
+++ src/Projects/BKDomain/Sources/Interface/Usecase/TermsAgreeUseCase.swift
@@ -0,0 +1,9 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import Combine
+import Foundation
+
+/// 유저가 약관동의를 진행했는지 여부를 서버에 전송합니다.
+public protocol TermsAgreeUseCase {
+    func execute(_ isAgreed: Bool) -> AnyPublisher<Bool, AuthError>
+}
