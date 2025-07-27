9e00bf3ffaf2a99a741226d8737e0e80a92a3523
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-27 18:08:36 +0900

    [BOOK-172] feat: add Route

--- src/Projects/BKPresentation/Sources/AuthFlow/Coordinator/LoginCoordinator.swift
+++ src/Projects/BKPresentation/Sources/AuthFlow/Coordinator/LoginCoordinator.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 import UIKit
+import SafariServices
 
 final class LoginCoordinator: Coordinator, FinishNotifying {
     weak var parentCoordinator: Coordinator?
@@ -21,5 +22,25 @@ final class LoginCoordinator: Coordinator, FinishNotifying {
         let loginViewController = LoginViewController(viewModel: LoginViewModel())
         loginViewController.coordinator = self
         navigationController.pushViewController(loginViewController, animated: true)
+    }
+}
+
+extension LoginCoordinator {
+    /// 약관동의 화면으로 이동
+    func goToTermsViewController() {
+        let termsViewController = TermsViewController(viewModel: TermsViewModel())
+        termsViewController.coordinator = self
+        navigationController.pushViewController(termsViewController, animated: true)
+    }
+    
+    /// MainFlow로 이동
+    func goToMainFlow() {
+        onFinish?()
+    }
+    
+    /// webview 보여주기 -> 추후 WebView로 전환해도됨(현재 사파리)
+    func showWebView(url: URL) {
+        let safariViewController = SFSafariViewController(url: url)
+        navigationController.present(safariViewController, animated: true)
     }
 }
