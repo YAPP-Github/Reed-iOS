8c5fd82752995d3a9abbc3d717d7d48e5b2a82f0
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-29 00:31:19 +0900

    [BOOK-174] feat: authStateUsecase 수정

--- src/Projects/BKDomain/Sources/Interface/Usecase/AuthStateUseCase.swift
+++ src/Projects/BKDomain/Sources/Interface/Usecase/AuthStateUseCase.swift
@@ -6,5 +6,5 @@ import Foundation
 /// 인증 상태를 체크합니다.
 /// 내부에 AccessToken이 있는지 확인하고, 서버로 보내 검증합니다.
 public protocol AuthStateUseCase {
-    func execute() -> AnyPublisher<Void, AuthError>
+    func execute() -> AnyPublisher<UserProfile, AuthError>
 }
