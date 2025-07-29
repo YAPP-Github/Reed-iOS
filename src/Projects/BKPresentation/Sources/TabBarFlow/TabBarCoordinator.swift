3b27ef451eb290bcf3d0207e934ab46edc825d81
Jiyong Jung <hickerd@sch.ac.kr>
2025-07-30 04:08:31 +0900

    [BOOK-184] fix: reflect review

--- src/Projects/BKPresentation/Sources/TabBarFlow/TabBarCoordinator.swift
+++ src/Projects/BKPresentation/Sources/TabBarFlow/TabBarCoordinator.swift
@@ -28,29 +28,22 @@ final class TabBarCoordinator: Coordinator, FinishNotifying {
 private extension TabBarCoordinator {
     func setupTabBarCoordinators() {
         let viewControllers: [UINavigationController] = TabItem.allCases.map { item in
-            let navController = UINavigationController()
-            let coordinator = item.makeCoordinator(parent: self, navigationController: navController)
+            let navigationController = UINavigationController()
+            let coordinator = item.makeCoordinator(parent: self, navigationController: navigationController)
 
             addChildCoordinator(coordinator)
             coordinator.start()
             
-            navController.tabBarItem = UITabBarItem(
+            navigationController.tabBarItem = UITabBarItem(
                 title: item.title,
                 image: item.icon.withTintColor(.bkBackgroundColor(.disable)),
                 selectedImage: item.icon.withTintColor(.bkContentColor(.primary))
             )
-            return navController
+            
+            return navigationController
         }
 
         tabBarController.viewControllers = viewControllers
-    }
-    
-    func setTabItem(_ item: TabItem) -> UITabBarItem {
-        return UITabBarItem(
-            title: item.title,
-            image: item.icon.withTintColor(.bkBackgroundColor(.disable)),
-            selectedImage: item.icon.withTintColor(.bkContentColor(.primary))
-        )
     }
 }
 
