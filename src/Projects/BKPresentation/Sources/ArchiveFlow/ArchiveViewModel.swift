e796716bbb756efb9f879045c8c205632e7a1347
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-31 21:36:48 +0900

    [BOOK-171] test: 가로스크롤 형식 테스트

--- src/Projects/BKPresentation/Sources/ArchiveFlow/ArchiveViewModel.swift
+++ src/Projects/BKPresentation/Sources/ArchiveFlow/ArchiveViewModel.swift
@@ -207,10 +207,10 @@ final class ArchiveViewModel: BaseViewModel {
     
     private func createMockChips() -> [ChipData] {
         return [
-            ChipData(title: "전체", count: 4),
-            ChipData(title: "읽기 전", count: 2),
-            ChipData(title: "읽는 중", count: 2),
-            ChipData(title: "완독", count: 0)
+            ChipData(title: "전체", count: 432342),
+            ChipData(title: "읽기 전", count: 2123),
+            ChipData(title: "읽는 중", count: 234),
+            ChipData(title: "완독", count: 100)
         ]
     }
     
