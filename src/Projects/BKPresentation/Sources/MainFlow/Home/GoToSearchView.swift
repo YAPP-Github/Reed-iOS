a6ca7383ee6d4d1a94893865324c0ee1f50d7eda
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-08-02 02:59:53 +0900

    [BOOK-209] feat: 서브뷰 먼저 개발

---
+++ src/Projects/BKPresentation/Sources/MainFlow/Home/GoToSearchView.swift
@@ -0,0 +1,41 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import BKDesign
+import SnapKit
+import UIKit
+
+final class GoToSearchView: UIView {
+    private let title = BKLabel(
+        text: "책 등록하기",
+        fontStyle: .body2(weight: .medium),
+        color: .bkContentColor(.brand)
+    )
+    
+    private let iconImage = UIImageView(image: BKImage.Icon.chevronRight)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setLayout()
+    }
+
+    private func setLayout() {
+        iconImage.tintColor = UIColor.bkContentColor(.brand)
+        
+        addSubviews(title, iconImage)
+        
+        title.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview()
+        }
+        
+        iconImage.snp.makeConstraints {
+            $0.leading.equalTo(title.snp.trailing).offset(BKSpacing.spacing1)
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(20)
+        }
+    }
+}
