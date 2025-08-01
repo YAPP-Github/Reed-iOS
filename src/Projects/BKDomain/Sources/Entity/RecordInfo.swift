1edfa29965d3a71c4e958eb69d60f3c5aebaf988
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-08-01 09:58:53 +0900

    [BOOK-199] feat: record entity, vo, repo 세팅
    레포 디테일한 부분 수정해야합니다.

---
+++ src/Projects/BKDomain/Sources/Entity/RecordInfo.swift
@@ -0,0 +1,43 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import Foundation
+
+public struct RecordInfo {
+    public let recordId: String
+    public let bookId: String
+    public let pageNumber: Int
+    public let quote: String?
+    public let review: String?
+    public let emotionTags: [String]
+    public let createdAt: Date
+    public let updatedAt: Date?
+    public let bookTitle: String
+    public let bookPublisher: String
+    public let bookCoverImageUrl: URL?
+    
+    public init(
+        recordId: String,
+        bookId: String,
+        pageNumber: Int,
+        quote: String?,
+        review: String?,
+        emotionTags: [String],
+        createdAt: Date,
+        updatedAt: Date?,
+        bookTitle: String,
+        bookPublisher: String,
+        bookCoverImageUrl: URL?
+    ) {
+        self.recordId = recordId
+        self.bookId = bookId
+        self.pageNumber = pageNumber
+        self.quote = quote
+        self.review = review
+        self.emotionTags = emotionTags
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.bookTitle = bookTitle
+        self.bookPublisher = bookPublisher
+        self.bookCoverImageUrl = bookCoverImageUrl
+    }
+}
