e786185b42ee68bae900c8d3be43604125b6045b
Jiyong Jung <hickerd@sch.ac.kr>
2025-08-02 05:15:57 +0900

    [BOOK-210] feat: implement CreateRecordUseCase

---
+++ src/Projects/BKDomain/Sources/Interface/Usecase/CreateRecordUseCase.swift
@@ -0,0 +1,10 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import Combine
+
+public protocol CreateRecordUseCase {
+    func execute(
+        bookId: String,
+        record: RecordVO
+    ) -> AnyPublisher<Void, Error>
+}
