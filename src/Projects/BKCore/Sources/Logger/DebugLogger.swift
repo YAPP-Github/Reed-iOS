72a1e9704f1b5fcbad4ac465eae616b189fb5349
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-08-05 02:31:46 +0900

    [BOOK-218] fix: pulse logger 수정

--- src/Projects/BKCore/Sources/Logger/DebugLogger.swift
+++ src/Projects/BKCore/Sources/Logger/DebugLogger.swift
@@ -6,6 +6,7 @@ import Foundation
     import Pulse
 #endif
 
+// nil값 검증도 가능하도록 optional로 message pass
 public func debugPulse(
     _ message: Any?...,
     function: String = #function,
@@ -14,7 +15,7 @@ public func debugPulse(
 ) {
     #if DEBUG
     LoggerStore.shared.storeMessage(
-        label: "console print",
+        label: "pulse log print",
         level: .debug,
         message: message.map({ String(describing: $0) }).joined(separator: " "),
         file: file,
