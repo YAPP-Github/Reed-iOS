7c513b4957ff516093c52883e727cc56af74819d
Jiyong Jung <hickerd@sch.ac.kr>
2025-08-04 23:47:18 +0900

    [BOOK-213] chore: add EmptyBook asset

--- src/Projects/BKDesign/Sources/Foundation/GraphicSystem/BKImage.swift
+++ src/Projects/BKDesign/Sources/Foundation/GraphicSystem/BKImage.swift
@@ -58,6 +58,7 @@ public enum BKImage {
         public static let onboarding2 = BKDesignAsset.onboarding2.image
         public static let homeChar = BKDesignAsset.homeCharacter.image
         public static let empty = BKDesignAsset.empty.image
+        public static let emptyBook = BKDesignAsset.emptybook.image
         public static let warmth = BKDesignAsset.warmth.image
         public static let sadness = BKDesignAsset.sadness.image
         public static let joy = BKDesignAsset.joy.image
