c1a2dff774787c7e48eb6f22aa0529b80c881cf7
Jiyong Jung <hickerd@sch.ac.kr>
2025-08-06 20:21:50 +0900

    [BOOK-226] refactor: update Entity and DTO to match actual API spec
    - Adjusted properties and types to align with server responses
    - Ensured decoding consistency between DTO and Entity

--- src/Projects/BKDomain/Sources/Entity/Book.swift
+++ src/Projects/BKDomain/Sources/Entity/Book.swift
@@ -6,23 +6,26 @@ public struct Book: Hashable {
     public let isbn: String
     public let title: String
     public let author: String
+    public let pubDate: Date?
     public let publisher: String
     public let thumbnail: URL?
-    public let userBookStatus: String
+    public let userBookStatus: BookStatus
     public let recordCount: Int?
     
     public init(
         isbn: String,
         title: String,
         author: String,
+        pubDate: Date? = nil,
         publisher: String,
         thumbnail: URL?,
-        userBookStatus: String,
+        userBookStatus: BookStatus,
         recordCount: Int? = nil
     ) {
         self.isbn = isbn
         self.title = title
         self.author = author
+        self.pubDate = pubDate
         self.publisher = publisher
         self.thumbnail = thumbnail
         self.userBookStatus = userBookStatus
