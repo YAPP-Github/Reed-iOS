89f0d57a67cb279babb477c86b96b5eac060c43b
Jiyong Jung <hickerd@sch.ac.kr>
2025-08-06 20:19:53 +0900

    [BOOK-226] refactor: update coordinator flow from Home to Record Detail screen

--- src/Projects/BKPresentation/Sources/MainFlow/Home/Coordinator/MainFlowCoordinator.swift
+++ src/Projects/BKPresentation/Sources/MainFlow/Home/Coordinator/MainFlowCoordinator.swift
@@ -54,10 +54,15 @@ extension MainFlowCoordinator {
         noteCoordinator.start()
     }
     
-    func didTapBookDetailButton() {
+    func didTapBookDetailButton(
+        isbn: String,
+        userBookId: String
+    ) {
         let bookDetailCoordinator = BookDetailCoordinator(
             parentCoordinator: self,
-            navigationController: navigationController
+            navigationController: navigationController,
+            isbn: isbn,
+            userBookId: userBookId
         )
         childCoordinators.append(bookDetailCoordinator)
         bookDetailCoordinator.start()
