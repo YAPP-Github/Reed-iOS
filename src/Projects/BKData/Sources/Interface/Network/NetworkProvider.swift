e446116cdd803faaf8c920ba325ad10341990e96
Jiyong Jung <hickerd@sch.ac.kr>
2025-08-08 19:12:28 +0900

    [BOOK-229] fix: adjust excessively long timeout duration

--- src/Projects/BKData/Sources/Interface/Network/NetworkProvider.swift
+++ src/Projects/BKData/Sources/Interface/Network/NetworkProvider.swift
@@ -1,6 +1,7 @@
 // Copyright © 2025 Booket. All rights reserved
 
 import Combine
+import Foundation
 
 public protocol NetworkProvider {
     @discardableResult
@@ -8,4 +9,24 @@ public protocol NetworkProvider {
         target: RequestTarget,
         type: T.Type
     ) -> AnyPublisher<T, NetworkError>
+}
+
+public extension NetworkProvider {
+    func mapToNetworkError(_ error: Error) -> NetworkError {
+        if let net = error as? NetworkError { return net }
+        if let urlError = error as? URLError {
+            switch urlError.code {
+            case .timedOut,
+                 .networkConnectionLost,
+                 .cannotFindHost,
+                 .cannotConnectToHost,
+                 .dnsLookupFailed,
+                 .notConnectedToInternet:
+                return .timeout
+            default:
+                return .unknown
+            }
+        }
+        return .unknown
+    }
 }
