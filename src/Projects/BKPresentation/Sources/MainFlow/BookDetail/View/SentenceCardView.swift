74b7156581875a52a564d016b480809c7c0db9d6
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-08-21 01:09:33 +0900

    [BOOK-281] feat: UI components 선언

---
+++ src/Projects/BKPresentation/Sources/MainFlow/BookDetail/View/SentenceCardView.swift
@@ -0,0 +1,15 @@
+// Copyright © 2025 Booket. All rights reserved
+
+import BKDesign
+import BKDomain
+import SnapKit
+import UIKit
+
+final class SentenceCardView: BaseView {
+    private lazy var emotionBackgroundImageView = UIImageView()
+    private lazy var sentenceLabel = BKLabel()
+    private lazy var titleLabel = BKLabel()
+    
+    private let guideLabel = BKLabel()
+    
+}
