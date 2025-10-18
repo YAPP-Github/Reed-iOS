9e55bf5149a783b710ca3bda0680e1ba4d581c44
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-10-18 16:03:56 +0900

    [BOOK-357] feat: bookStatus의 String 반환 함수 추가

--- src/Projects/BKPresentation/Sources/MainFlow/Search/VO/BookRegistrationStatus.swift
+++ src/Projects/BKPresentation/Sources/MainFlow/Search/VO/BookRegistrationStatus.swift
@@ -28,4 +28,35 @@ enum BookRegistrationStatus: String {
             return .after
         }
     }
+    
+    func getSubTitle() -> String {
+        switch self {
+        case .before:
+            return "책을 읽으면서 독서 기록을 남길 수 있어요"
+        case .inProgress:
+            return "독서 기록을 바로 시작할까요?"
+        case .after:
+            return "기억에 남은 문장이나 감상을 기록해보세요"
+        }
+    }
+    
+    func getCancelButtonTitle() -> String {
+        switch self {
+        case .before:
+            return "확인"
+        case .inProgress, .after:
+            return "나중에 하기"
+        }
+    }
+    
+    func getNextButtonTitle() -> String {
+        switch self {
+        case .inProgress:
+            return "기록 시작하기"
+        case .after:
+            return "기록 남기기"
+        default:
+            return ""
+        }
+    }
 }
