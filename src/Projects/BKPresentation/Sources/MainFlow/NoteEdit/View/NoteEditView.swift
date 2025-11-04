a4d417326873e4adbac9ac0e24a6a5b06c6aa8a2
Jiyong Jung <hickerd@sch.ac.kr>
2025-11-04 18:14:57 +0900

    [BOOK-411] fix: update fonts in NoteEditView.pageField

--- src/Projects/BKPresentation/Sources/MainFlow/NoteEdit/View/NoteEditView.swift
+++ src/Projects/BKPresentation/Sources/MainFlow/NoteEdit/View/NoteEditView.swift
@@ -32,7 +32,8 @@ final class NoteEditView: BaseView {
     
     private let pageField = BKTextFieldView(
         labelText: "책 페이지",
-        placeholder: "기록하고 싶은 페이지를 작성해보세요"
+        placeholder: "기록하고 싶은 페이지를 작성해보세요",
+        fontStyle: .body2(weight: .medium)
     )
     
     private let sentenceTextView = BKTextView(
