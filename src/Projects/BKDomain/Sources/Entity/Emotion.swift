1fb8922334fc6dc1235d491e401d942e1b86de2f
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-11-07 08:25:26 +0900

    [BOOK-421] fix: Emotion Edit 로직 수정

--- src/Projects/BKDomain/Sources/Entity/Emotion.swift
+++ src/Projects/BKDomain/Sources/Entity/Emotion.swift
@@ -1,6 +1,6 @@
 // Copyright © 2025 Booket. All rights reserved
 
-public enum Emotion: String, CaseIterable, Decodable {
+public enum Emotion: String, CaseIterable, Decodable, Equatable {
     case warmth = "따뜻함"
     case joy = "즐거움"
     case sad = "슬픔"
